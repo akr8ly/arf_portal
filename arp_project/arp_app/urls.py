83d3fa6bc6570583ca1e7e9ace2d109b6f963d6a
akr8ly <avialok05@gmail.com>
2025-06-25 11:46:49 +0530

    new chapter4 page

--- arp_project/arp_app/urls.py
+++ arp_project/arp_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('chapter5/', views.chapter5_view, name='chapter5'),
+    path('chapter4/', views.chapter4_view, name='chapter4'),
     path('login/', views.login_view, name='login'),
 
 ]
--- arp_project/arp_app/views.py
+++ arp_project/arp_app/views.py
@@ -17,12 +17,17 @@ def login_view(request):
                 login(request, user)
 
                 role = user.role.role.strip().lower()
-                division = user.division.division.strip() if user.division else None
+                division = user.division.division.strip() if user.division else ""
+
+                print("ROLE:", role)
+                print("DIVISION:", division)
 
                 if role == 'admin':
                     return redirect(reverse('admin:index'))
                 elif role == 'filling_user' and division == 'Water, Air and Noise Monitoring Network':
                     return redirect('chapter5')
+                elif role == 'filling_user' and division == 'Committees Constituted by the Central Board  and their Activities':
+                    return redirect('chapter4')
                 
             else:
                 print("Incorrect password")
@@ -37,8 +42,10 @@ def login_view(request):
 def chapter5_view(request):
     return render(request, 'arp_app/chapter5.html')
 
+def chapter4_view(request):
+    return render(request, 'arp_app/chapter4.html')
+
 
 def home_view(request):
     return render(request, 'arp_app/home.html')
-
 
